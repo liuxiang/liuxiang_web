4888cffbcf3539d364e6a1740562af61c47e3f04
liuxiang <liuxiang.1227@qq.com>
2016-10-11 17:11:57 +0800

    add babel,es6-modules

---
+++ src/babel/test.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2016/10/11 0011.
+ */
+(function () {
+  'use strict';
+
+  import $ from 'jquery';
+  $('body').css('background-color', 'red');
+
+  console.log('babel run');
+
+})();
